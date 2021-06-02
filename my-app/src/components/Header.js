8021789794c776f1fdfcf5b81472780173037b51
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-06-02 13:27:18 +0200

    Add components

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <a href="./index.html">
+        {" "}
+        <div className="header__logo"></div>
+      </a>
+    </header>
+  );
+}
+
+export default Header;
