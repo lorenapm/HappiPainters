32683c571306aef3c0b80035ab890f474836bec3
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-02 14:20:21 +0200

    delete my-app folder

---
+++ version-0/src/js/03-data.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+//formulario
+const form = document.querySelector('.js-data');
+
+//nombre
+const nameInput = document.querySelector('.js-nameInput');
+const previewName = document.querySelector('.js-name');
+const nameDefault = 'Nombre y Apellido';
+
+//cargo
+const jobInput = document.querySelector('.js-job');
+const previewJob = document.querySelector('.js-jobTitle');
+const jobDefault = 'Front-end developer';
+
+//email
+const emailInput = document.querySelector('.js-email');
+const defaultEmail = 'mailto:';
+const previewEmail = document.querySelector('.js-buttonEm');
+
+//teléfono
+const telephoneInput = document.querySelector('.js-telephone');
+const defaultTelephone = 'tel:';
+const previewTel = document.querySelector('.js-buttonTel');
+
+//linkedin
+const linkedInInput = document.querySelector('.js-linkedin');
+const defaultUrlLinkedin = 'https://www.linkedin.com/in/';
+const previewLinkedIn = document.querySelector('.js-buttonLink');
+
+//github
+const githubInput = document.querySelector('.js-github');
+const defaultUrlGitHub = 'https://github.com/';
+const previewGit = document.querySelector('.js-buttonGit');
+
+//reset
+const resetButton = document.querySelector('.js-reset');
+
+//objeto
+let data = {
+  palette: '1',
+  name: '',
+  job: '',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: '',
+  photo: '',
+};
+
+//funciones
+function dataForm(ev) {
+  const inputAttribute = ev.target.name;
+  const userValue = ev.target.value;
+  data[inputAttribute] = userValue;
+}
+
+function previewCard() {
+  previewName.innerHTML = data.name === '' ? nameDefault : data.name;
+  previewJob.innerHTML = data.job === '' ? jobDefault : data.job;
+  previewEmail.href =
+    data.email === '' ? defaultEmail : defaultEmail + data.email;
+  previewTel.href =
+    data.phone === '' ? defaultTelephone : defaultTelephone + data.phone;
+  previewLinkedIn.href =
+    data.linkedin === ''
+      ? defaultUrlLinkedin
+      : defaultUrlLinkedin + data.linkedin;
+  previewGit.href =
+    data.github === '' ? defaultUrlGitHub : defaultUrlGitHub + data.github;
+}
+
+function handlerData(ev) {
+  dataForm(ev);
+  previewCard();
+  setlocalHost();
+  allowButton();
+}
+
+//eventos
+form.addEventListener('keyup', handlerData);
