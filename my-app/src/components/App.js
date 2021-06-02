ecbcbfc27f1405577914b4cac3a55bb5a7961086
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-06-02 12:37:38 +0200

    add copyright

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -256,7 +256,10 @@ function App() {
         </div>
       </main>
       <footer className="footer">
-        <small className="footer__info"> Awesome profile-cards #169 2021</small>
+        <small className="footer__info">
+          {" "}
+          Awesome profile-cards &copy; 2021
+        </small>
         <img className="footer__logo" src={logo} alt="Logo Adalab" />
       </footer>
     </>
